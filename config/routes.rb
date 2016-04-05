78c4a7e602ca442595e33f0176b3178afab660b6
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-04 18:01:55 -0600

    Begin basic routing for omniauth login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
 
+  root 'games#index'
+
+  resources :games, only: [:index, :show]
+  resources :users, only: [:index, :show]
+
+  get '/auth/twitch', as: :twitch_login
+  get '/auth/twitch/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: "logout"
 end
