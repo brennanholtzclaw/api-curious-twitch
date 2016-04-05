f8999b3b86cdb335cd585ece3df4d4dc2d824b89
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-04 21:40:43 -0600

    Generate Sessions Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   get 'sessions/create'
 
+  get 'sessions/destroy'
+
+  get 'sessions/create'
+
   root 'games#index'
 
   resources :games, only: [:index, :show]
-  resources :users, only: [:index, :show]
+  # resources :users, only: [:index, :show]
 
   get '/auth/twitch', as: :twitch_login
   get '/auth/twitch/callback', to: 'sessions#create'
