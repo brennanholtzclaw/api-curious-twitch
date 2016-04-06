bd9722e71aa439b300da19b7777deef428768df2
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-05 22:23:16 -0600

    Initial basic methods writted

--- app/services/twitch_service.rb
+++ app/services/twitch_service.rb
@@ -2,11 +2,16 @@ class TwitchService
 
   def initialize
     @headers = {"Client-ID" => "si0orm5co5ag077jshcfjrbhurdqp6u", "Accept" => "application/vnd.twitchtv.v2+json"}
+    @user_headers = {"Client-ID" => "si0orm5co5ag077jshcfjrbhurdqp6u", "Authorization" => "OAuth"}
     @host = "https://api.twitch.tv/kraken"
   end
 
   def get(path)
     HTTParty.get(prepare_path(path), headers: @headers)
+  end
+
+  def user_get(path)
+    HTTParty.get(prepare_path(path), headers: @user_headers)
   end
 
   def get_hash(path)
@@ -19,6 +24,11 @@ class TwitchService
 
   def top_games(num = 10)
     get("/games/top?limit=#{num}")
+  end
+
+  def followed_by_user(user, num=9)
+    @user_headers["Authorization"] = "OAuth #{user.oauth_token}"
+    user_get("/streams/followed?limit=#{num}")
   end
 
   def post(path, params)
